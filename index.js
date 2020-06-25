7edd85707cecb1cdaf08cfd47965563536eddc3d
lydelltyler <lydellgrowen@gmail.com>
2020-06-25 11:24:46 -0500

    fixed hover images

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 $('.img1').hover(function () {
-    $(this).attr('src', '/image/forest6.jpg')
+    $(this).attr('src', 'image/forest6.jpg')
 },
     $(".img1").mouseleave(function () {
-        $(this).attr('src', '/image/forest1.jpg')
+        $(this).attr('src', 'image/forest1.jpg')
     }))
 
 $('.img2').hover(function () {
-    $(this).attr('src', '/image/forest7.jpg')
+    $(this).attr('src', 'image/forest7.jpg')
 },
     $(".img2").mouseleave(function () {
-        $(this).attr('src', '/image/forest2.jpg')
+        $(this).attr('src', 'image/forest2.jpg')
     }))
 
 $('.img3').hover(function () {
-    $(this).attr('src', '/image/forest8.jpg')
+    $(this).attr('src', 'image/forest8.jpg')
 },
     $(".img3").mouseleave(function () {
-        $(this).attr('src', '/image/forest3.jpg')
+        $(this).attr('src', 'image/forest3.jpg')
     }))
 
 $('.img4').hover(function () {
-    $(this).attr('src', '/image/forest6.jpg')
+    $(this).attr('src', 'image/forest6.jpg')
 },
     $(".img4").mouseleave(function () {
-        $(this).attr('src', '/image/forest4.jpg')
+        $(this).attr('src', 'image/forest4.jpg')
     }))
 
